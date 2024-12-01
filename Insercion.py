666aa4f9f3938654a6afc950c70fc5df19c88760
LiuDoni2 <ld.unknown.2001@gmail.com>
2024-11-30 21:56:09 -0500

    Update Insercion.py

--- Insercion.py
+++ Insercion.py
@@ -1,16 +1,15 @@
 def insercion(lista):
-    for i in range(1, len(lista)):
-        clave = lista[i]  
+    for i in range(1, len(lista)): 
+        valor_actual = lista[i]
         j = i - 1
-        while j >= 0 and lista[j] > clave:
-            lista[j + 1] = lista[j]
+        while j >= 0 and lista[j] > valor_actual:  
+            lista[j + 1] = lista[j] 
             j -= 1
-        lista[j + 1] = clave 
-        print(f"Después de la inserción {i}: {lista}")
+        lista[j + 1] = valor_actual  
 
 print("Ordenamiento por Inserción - Insertion Sort")
-n = int(input("Ingrese el número de elementos: "))
-lista = [int(input(f"Ingrese el número en la posición {k + 1}: ")) for k in range(n)]
+n = int(input("Ingrese el número de elementos: "))  
+lista = [int(input(f"Ingrese el elemento {k + 1}: ")) for k in range(n)]  
 
 print("Lista original:", lista)
 insercion(lista)
